5ed563dfedf030bc312d45e73fd4a33cc544071b
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-05 20:40:55 -0700

    login de usuario.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public status: string;
+  public identity;
+  public token;
 
-  constructor() { }
+  constructor(
+    private _userService: UserService
+  ) { 
+    this.page_title = "Identificate";
+    this.user = new User('','','','','','','ROLE_USER');
+    
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    // Conseguir objeto del usuario logueado
+    this._userService.signup(this.user).subscribe(
+      response =>{
+        if(response.user && response.user._id){
+          this.status = 'success';
+          
+          // Guardamos el usuario en una propiedad
+          this.identity  = response.user;
+
+          // Conseguir el token del usuario identificado
+            this._userService.signup(this.user, true).subscribe(
+              response =>{
+                if(response.token){
+                  
+                  // Guardar el token del usuario en una propiedad
+                  this.token = response.token;
+        
+                }else{
+                  this.status = 'error';
+                }
+              },
+              error =>{
+                this.status = 'error';
+                console.log(<any>error);
+              });
+
+        }else{
+          this.status = 'error';
+        }
+      },
+      error =>{
+        this.status = 'error';
+        console.log(<any>error);
+      });
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -23,4 +23,18 @@ export class UserService{
         // Hacer peticion ajax
         return this._http.post(this.url+'register', params,{headers: headers});
     }
+
+    signup (user, gettoken = null):Observable<any>{
+        //comprobar si llega el gettoken
+        if(gettoken !=null){
+            user.gettoken = gettoken;
+        }
+
+         // Convertir el objeto del usuario a un json string
+        let params = JSON.stringify(user);
+        // Definir las cabeceras
+        let headers = new HttpHeaders().set('Content-Type', 'Application/json');
+
+        return this._http.post(this.url+'login', params,{headers: headers});
+    }
 }
