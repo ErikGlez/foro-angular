aa54e422211cd9e7cb2300b7e7074d6b643945f2
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-06 17:28:37 -0700

    LocalStorage y cerrar sesión.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params}  from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { from } from 'rxjs';
 
 
 
@@ -18,7 +20,9 @@ export class LoginComponent implements OnInit {
   public token;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) { 
     this.page_title = "Identificate";
     this.user = new User('','','','','','','ROLE_USER');
@@ -37,6 +41,8 @@ export class LoginComponent implements OnInit {
           
           // Guardamos el usuario en una propiedad
           this.identity  = response.user;
+          // Guardamos el objeto del usuario en la sesión
+          localStorage.setItem('identity',JSON.stringify(this.identity));
 
           // Conseguir el token del usuario identificado
             this._userService.signup(this.user, true).subscribe(
@@ -45,6 +51,10 @@ export class LoginComponent implements OnInit {
                   
                   // Guardar el token del usuario en una propiedad
                   this.token = response.token;
+                  localStorage.setItem('token', this.token);
+
+                  this.status = 'success';
+                  this._router.navigate(['/inicio']);
         
                 }else{
                   this.status = 'error';
@@ -58,6 +68,8 @@ export class LoginComponent implements OnInit {
         }else{
           this.status = 'error';
         }
+
+        form.reset();
       },
       error =>{
         this.status = 'error';
