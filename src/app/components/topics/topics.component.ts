5472be40aa5fe57671587e87526308af0b8cc899
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-10 20:14:39 -0700

    Paginado del listado de temas.

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -18,6 +18,7 @@ export class TopicsComponent implements OnInit {
   public page;
   public next_page;
   public prev_page;
+  public number_pages;
 
   constructor(
     private _topicService: TopicService,
@@ -28,7 +29,16 @@ export class TopicsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getTopics(1);
+    this._route.params.subscribe(params=>{
+      var page = +params['page'];
+
+      if(!page || page == null  || page == undefined){
+        this.prev_page =1;
+        this.next_page = 2;
+      }
+      this.getTopics(page);
+    });
+    
   }
 
   getTopics(page = 1){
@@ -38,6 +48,26 @@ export class TopicsComponent implements OnInit {
           this.topics = response.topics;
 
           // Navegacion de paginación
+          this.totalPages = response.totalPages;
+          
+          var number_pages = [];
+          for(var i = 1; i<= this.totalPages; i++){
+            number_pages.push(i);
+          }
+          this.number_pages = number_pages;
+
+          if(page >= 2){
+            this.prev_page = page -1;
+          }else{
+            this.prev_page = 1;
+          }
+
+          if(page < this.totalPages){
+            this.next_page = page +1;
+          }else{
+            this.next_page = this.totalPages;
+          }
+
         }else{
           this._router.navigate(['/inicio']);
         }
