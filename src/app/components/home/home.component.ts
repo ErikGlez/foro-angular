e6469f763b899ad0bbd26d4e317d1e502a6d7416
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-11-11 16:17:26 -0700

    Redirecciones en la home page.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [UserService]
 })
 export class HomeComponent implements OnInit {
+
+  public identity;
   public page_title: string;
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Bienvenido al foro de programación'
+    this.identity = this._userService.getIdentity();
    }
 
   ngOnInit() {
+  }
+
+  ngDoCheck(){
+    this.identity = this._userService.getIdentity();
   }
 
 }
